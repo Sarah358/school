00cc1d562d6c87382b94d9ae6f5d7f7798c96110
sarah njeri <60597568+Sarah358@users.noreply.github.com>
2021-05-02 12:14:41 +0300

    'home-page'

---
+++ js/style.js
@@ -0,0 +1,12 @@
+console.log('hello');
+
+let navLinks = document.getElementById('navLinks');
+
+function hideMenu(){
+    navLinks.style.display= "none";
+}
+function showMenu(){
+    navLinks.style.display = 'block';
+}
+
+ 
